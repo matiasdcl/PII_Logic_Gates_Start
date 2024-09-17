2efb71697568a6f18ebddd581428665ae1727c7a
Ines Barral <barral.mariaines@gmail.com>
2024-09-17 17:12:33 -0300

    Merged not tests

--- src/Library/NOTgate.cs
+++ src/Library/NOTgate.cs
@@ -4,11 +4,20 @@ public class NOTgate : IValorVerdad
 {
     private List<IValorVerdad> Entradas = new List<IValorVerdad>();
 
+    public List<IValorVerdad> GetEntradas()
+    {
+        return Entradas;
+    }
+    
     public void AgregarValor(IValorVerdad valor)
     {
         if (Entradas.Count == 0)
         {
          Entradas.Add(valor);   
+        }
+        else
+        {
+            Console.WriteLine("La compuerta Not solo admite una entrada");
         }
     }
 
