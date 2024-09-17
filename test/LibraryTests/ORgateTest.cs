55e38ac2f03ef090172df1701c899c949f1f14fe
FacundoPiriz17 <facundo.piriz@correo.ucu.edu.uy>
2024-09-17 17:12:23 -0300

    Test de OR finalizados

--- test/LibraryTests/ORgateTest.cs
+++ test/LibraryTests/ORgateTest.cs
@@ -3,13 +3,62 @@ using Library;
 namespace LibraryTests;
 
 [TestFixture]
-[TestOf(typeof(ORgate))]
 public class ORgateTest
 {
+    
+    [Test]
+    public void TestAgregarValor()
+    {
+        ORgate or = new ORgate();
+        IValorVerdad a = new ValorVerdadTrue();
+        or.AgregarValor(a);
+        Assert.That(or.GetEntradas().Contains(a));
+        IValorVerdad b = new ValorVerdadFalse();
+        or.AgregarValor(b);
+        Assert.That(or.GetEntradas().Contains(b));
+    }
 
     [Test]
-    public void METHOD()
+    public void TestComportamientoTrueTrue()
     {
-        
+        ORgate or = new ORgate();
+        IValorVerdad a = new ValorVerdadTrue();
+        IValorVerdad b = new ValorVerdadTrue();
+        or.AgregarValor(a);
+        or.AgregarValor(b);
+        Assert.AreEqual(true,or.Calcular());
+    }
+    
+    [Test]
+    public void TestComportamientoFalseTrue()
+    {
+        ORgate or = new ORgate();
+        IValorVerdad a = new ValorVerdadFalse();
+        IValorVerdad b = new ValorVerdadTrue();
+        or.AgregarValor(a);
+        or.AgregarValor(b);
+        Assert.AreEqual(true,or.Calcular());
+    }
+    
+    [Test]
+    public void TestComportamientoTrueFalse()
+    {
+        ORgate or = new ORgate();
+        IValorVerdad a = new ValorVerdadTrue();
+        IValorVerdad b = new ValorVerdadFalse();
+        or.AgregarValor(a);
+        or.AgregarValor(b);
+        Assert.AreEqual(true,or.Calcular());
+    }
+    
+    [Test]
+    public void TestComportamientoFalseFalse()
+    {
+        ORgate or = new ORgate();
+        IValorVerdad a = new ValorVerdadFalse();
+        IValorVerdad b = new ValorVerdadFalse();
+        or.AgregarValor(a);
+        or.AgregarValor(b);
+        Assert.AreEqual(false,or.Calcular());
     }
 }
