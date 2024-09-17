a5c8383d5c2c08648a9d1f3d1c26c4f611113ba8
Ines Barral <barral.mariaines@gmail.com>
2024-09-17 15:52:18 -0300

    Se agrega clase NotGate

--- src/Library/NOTgate.cs
+++ src/Library/NOTgate.cs
@@ -2,5 +2,18 @@ namespace Library;
 
 public class NOTgate : IValorVerdad
 {
+    private List<IValorVerdad> Entradas = new List<IValorVerdad>();
 
+    public void AgregarValor(IValorVerdad valor)
+    {
+        if (Entradas.Count == 0)
+        {
+         Entradas.Add(valor);   
+        }
+    }
+
+    public bool Calcular()
+    {
+        return !Entradas[1].Calcular();
+    }
 }
