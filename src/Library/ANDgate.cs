49db43abb046a89a14fc1524bdb777df5a4c5eb5
unknown <mateo.gutierrezo@correo.ucu.edu.uy>
2024-09-17 15:47:11 -0300

    ANDgate

--- src/Library/ANDgate.cs
+++ src/Library/ANDgate.cs
@@ -2,5 +2,20 @@
 
 public class ANDgate : IValorVerdad
 {
+    private List<IValorVerdad> entradas  = new List<IValorVerdad>();
+
+    public void AddEntrada(IValorVerdad valorVerdad)
+    {
+        this.entradas.Add(valorVerdad);    
+    }
     
+    public bool Calcular()
+    {
+        bool result = true;
+        foreach (IValorVerdad valor in entradas)
+        {
+            result = result && valor.Calcular();
+        }
+        return result;
+    }
 }
